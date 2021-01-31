33cb630d79392cad7d2f80dd4f918adbdfbd7181
parth sarthi prasad <mepsp2000@gmail.com>
2021-01-31 19:34:28 +0530

    completed pickle server and standard deviation with data normalisation

--- server.py
+++ server.py
@@ -6,7 +6,10 @@ from flask import Flask, request
 
 model = None
 app = Flask(__name__)
+meanVal = np.array([183.688783,183.000054,983.063901,4.100918])
+stdDeviation = np.array([83.070587,82.326782,1.664243,1.454851])
 
+#todo add weather data correction in the implementation
 
 def load_model():
     global model
@@ -26,11 +29,16 @@ def get_prediction():
     if request.method == 'POST':
         data = request.get_json()  # Get data posted as a json
         data = np.array(data)[np.newaxis, :]  # converts shape from (4,) to (1, 4)
-        print(data)
+        for i in range(data.shape[0]):
+            data[i] = (data[i] - meanVal[i])/stdDeviation[i]
+        # print(data)
         prediction = model.predict(data)  # runs globally loaded model on the data
-    return str(prediction[0])
+        terr = (prediction[0]/15)
+        terr = min(terr,1)
+        # print(terr)
+    return str(terr)
 
 
 if __name__ == '__main__':
     load_model()  # load model at the beginning once only
-    app.run(ssl_context='adhoc', host='127.0.0.1', port=5555)
+    app.run(ssl_context='adhoc', host='127.0.0.1', port=5556)
