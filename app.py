fixed indexing and iteration of request and data normalisation

app.py
@@ -29,8 +29,11 @@ def get_prediction():
     if request.method == 'POST':
         data = request.get_json()  # Get data posted as a json
         data = np.array(data)[np.newaxis, :]  # converts shape from (4,) to (1, 4)
-        for i in range(data.shape[0]):
-            data[i] = (data[i] - meanVal[i])/stdDeviation[i]
+        for i in range(4):
+            data[0][i] = ((data[0][i]) - (meanVal[i]))/(stdDeviation[i])
+            # print(data.shape)
+            # print(str(i)+" "+str(data[0][i])+" "+str(meanVal[i])+" "+str(stdDeviation[i]))
+            # print()
         # print(data)
         prediction = model.predict(data)  # runs globally loaded model on the data
         terr = (prediction[0]/15)